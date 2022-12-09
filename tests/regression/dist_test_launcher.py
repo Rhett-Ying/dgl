b640efa0e6709e1b8fd2ae5cbb543870f5cc100d
RhettYing <rhett_ying@qq.com>
2022-12-09 06:04:11 +0000

    update

--- tests/regression/dist_test_launcher.py
+++ tests/regression/dist_test_launcher.py
@@ -14,17 +14,46 @@ def prepare_env():
         os.makedirs(workspace)
         os.environ['WORKSPACE'] = workspace
     os.environ['IP_CONFIG'] = os.path.join(workspace, 'ip_config.txt')
-    os.environ['SSH_PORT'] = 2233
+    os.environ['SSH_PORT'] = '2233'
 
 def fetch_raw_data():
     workspace = os.environ.get('WORKSPACE', '/workspace')
+    data_path = os.path.join(workspace, 'test_dataset')
     os.system(
-        'aws s3 sync s3://dgl-data-store/test_dataset '
-        f'{workspace}/test_dataset'
+        f'aws s3 sync s3://dgl-data-store/test_dataset {data_path}'
     )
     os.system(
-        f'ls -lh {workspace}/test_dataset'
+        f'ls -lh {data_path}'
     )
+    return data_path
+
+def graph_partition(root_dir):
+    num_parts = 4
+
+    # Step1: graph partition
+    in_dir = os.path.join(root_dir, "chunked-data")
+    output_dir = os.path.join(root_dir, "parted_data")
+    os.system(
+        "python3 /dgl/tools/partition_algo/random_partition.py "
+        "--in_dir {} --out_dir {} --num_partitions {}".format(
+            in_dir, output_dir, num_parts
+        )
+    )
+
+    # Step2: data dispatch
+    partition_dir = os.path.join(root_dir, 'parted_data')
+    out_dir = os.path.join(root_dir, 'partitioned')
+    ip_config = os.environ['IP_CONFIG']
+
+    cmd = "python3 /dgl/tools/dispatch_data.py"
+    cmd += f" --in-dir {in_dir}"
+    cmd += f" --partitions-dir {partition_dir}"
+    cmd += f" --out-dir {out_dir}"
+    cmd += f" --ip-config {ip_config}"
+    cmd += f" --ssh-port {os.environ['SSH_PORT']}"
+    cmd += " --process-group-timeout 60"
+
+    os.system(cmd)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -45,4 +74,7 @@ if __name__ == '__main__':
     )
 
     # fetch raw data
-    fetch_raw_data()
+    data_path = fetch_raw_data()
+
+    # graph partition
+    graph_partition(data_path)
