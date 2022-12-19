36f8b04b488a6853e0cc42a87fb35655a7898acc
RhettYing <rhett_ying@qq.com>
2022-12-19 08:43:48 +0000

    sync before run

--- tests/regression/task/task.py
+++ tests/regression/task/task.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import time
+import pathlib
 
 
 class Task:
@@ -17,10 +18,24 @@ class Task:
         raise RuntimeError("Not implemented...")
 
     def _continue_on_main_nodes_only(self):
+        workspace = os.environ['WORKSPACE']
+        sync_file = os.path.join(workspace, 'dist_dgl_node_ready.sync')
+        pathlib.Path(sync_file).touch()
         if os.environ["AWS_BATCH_JOB_MAIN_NODE_INDEX"] != \
             os.environ["AWS_BATCH_JOB_NODE_INDEX"]:
             logging.info("Child node goes to sleep now...")
             time.sleep(60*60*24)
+        ip_config = os.environ['IP_CONFIG']
+        ssh_port = os.environ['SSH_PORT']
+        with open(ip_config, 'r') as f:
+            for line in f:
+                ip = line.rstrip()
+                logging.info(f"-------IP: {ip}")
+                cmd = f'while [ ! -f {sync_file} ]; do sleep 2; done'
+                os.system(
+                    f"ssh -o StrictHostKeyChecking=no -p {ssh_port} {ip} '{cmd}'"
+                )
+                logging.info(f"{ip} is in sleep now...")
         logging.info("Main node continues...")
 
     def _do_run(self):
