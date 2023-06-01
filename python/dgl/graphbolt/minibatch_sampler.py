a1ddaa9f57fe9cc513ddf9ccf8ddef9a078f2b8b
RhettYing <rhett_ying@qq.com>
2023-06-01 06:56:25 +0000

    add tests

--- python/dgl/graphbolt/minibatch_sampler.py
+++ python/dgl/graphbolt/minibatch_sampler.py
@@ -1,13 +1,15 @@
 """Minibatch Sampler"""
 
+from functools import partial
 from typing import Optional
 
 import torch
+from torch.utils.data import default_collate
 from torchdata.datapipes import functional_datapipe
 from torchdata.datapipes.iter import IterDataPipe
-
 from .itemset import *
-
+from ..batch import batch as dgl_batch
+from ..heterograph import DGLGraph
 
 __all__ = ["MinibatchSampler"]
 
@@ -23,6 +25,29 @@ class DGLMinibatcherIterDataPipe(IterDataPipe):
     def __iter__(self):
         for item in self.item_set:
             yield item
+
+
+def colloate(batch):
+    data = next(iter(batch))
+    if isinstance(data, DGLGraph):
+        return dgl_batch(batch)
+    elif isinstance(data, dict):
+        def _wrapper(batch):
+            if isinstance(batch, list):
+                return tuple(batch)
+            return batch
+        collected_batch = {}
+        for b in batch:
+            b_key = list(b.keys())[0]
+            b_value = list(b.values())[0]
+            if b_key not in collected_batch:
+                collected_batch[b_key] = []
+            collected_batch[b_key].append(b_value)
+        return {
+            key: _wrapper(default_collate(value))
+            for key, value in collected_batch.items()
+        }
+    return default_collate(batch)
 
 
 class MinibatchSampler:
@@ -51,6 +76,8 @@ class MinibatchSampler:
         data_pipe = data_pipe.batch(
             batch_size=self.batch_size,
             drop_last=self.drop_last,
-            wrapper_class=torch.Tensor,
+            # wrapper_class=torch.Tensor,
+            # wrapper_class=DGLGraph,
         )
+        data_pipe = data_pipe.collate(collate_fn=colloate)
         return iter(data_pipe)
--- tests/python/pytorch/graphbolt/test_minibatch_sampler.py
+++ tests/python/pytorch/graphbolt/test_minibatch_sampler.py
@@ -1,151 +1,412 @@
 import dgl
 import pytest
 import torch
+from dgl import graphbolt as gb
 from torch.testing import assert_close
-from dgl.graphbolt import *
 
 
 @pytest.mark.parametrize("batch_size", [1, 4])
 @pytest.mark.parametrize("shuffle", [True, False])
 @pytest.mark.parametrize("drop_last", [True, False])
-def test_MinibatchSampler_node_edge_ids(batch_size, shuffle, drop_last):
+def test_ItemSet_node_edge_ids(batch_size, shuffle, drop_last):
     # Node or edge IDs.
-    num_ids = 11
-    item_set = ItemSet(torch.arange(0, num_ids))
-    minibatch_sampler = MinibatchSampler(
+    num_ids = 103
+    item_set = gb.ItemSet(torch.arange(0, num_ids))
+    minibatch_sampler = gb.MinibatchSampler(
         item_set, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last
     )
-    items = []
-    for i, item in enumerate(minibatch_sampler):
+    minibatch_ids = []
+    for i, minibatch in enumerate(minibatch_sampler):
         is_last = (i + 1) * batch_size >= num_ids
         if not is_last or num_ids % batch_size == 0:
-            assert len(item) == batch_size
+            assert len(minibatch) == batch_size
         else:
             if not drop_last:
-                assert len(item) == num_ids % batch_size
+                assert len(minibatch) == num_ids % batch_size
             else:
                 assert False
-        items.append(item)
-    items = torch.cat(items)
-    assert torch.all(items[:-1] <= items[1:]) is not shuffle
+        minibatch_ids.append(minibatch)
+    minibatch_ids = torch.cat(minibatch_ids)
+    assert torch.all(minibatch_ids[:-1] <= minibatch_ids[1:]) is not shuffle
 
 
-"""
-def test_ItemSet_graphs():
+@pytest.mark.parametrize("batch_size", [1, 4])
+@pytest.mark.parametrize("shuffle", [True, False])
+@pytest.mark.parametrize("drop_last", [True, False])
+def test_ItemSet_graphs(batch_size, shuffle, drop_last):
     # Graphs.
-    graphs = [dgl.rand_graph(10, 20) for _ in range(5)]
-    item_set = ItemSet(graphs)
-    for i, item in enumerate(item_set):
-        assert graphs[i] == item
+    num_graphs = 103
+    num_nodes = 10
+    num_edges = 20
+    graphs = [
+        dgl.rand_graph(num_nodes * (i + 1), num_edges * (i + 1))
+        for i in range(num_graphs)
+    ]
+    item_set = gb.ItemSet(graphs)
+    minibatch_sampler = gb.MinibatchSampler(
+        item_set, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last
+    )
+    minibatch_num_nodes = []
+    minibatch_num_edges = []
+    for i, minibatch in enumerate(minibatch_sampler):
+        is_last = (i + 1) * batch_size >= num_graphs
+        if not is_last or num_graphs % batch_size == 0:
+            assert minibatch.batch_size == batch_size
+        else:
+            if not drop_last:
+                assert minibatch.batch_size == num_graphs % batch_size
+            else:
+                assert False
+        minibatch_num_nodes.append(minibatch.batch_num_nodes())
+        minibatch_num_edges.append(minibatch.batch_num_edges())
+    minibatch_num_nodes = torch.cat(minibatch_num_nodes)
+    minibatch_num_edges = torch.cat(minibatch_num_edges)
+    assert (
+        torch.all(minibatch_num_nodes[:-1] <= minibatch_num_nodes[1:])
+        is not shuffle
+    )
+    assert (
+        torch.all(minibatch_num_edges[:-1] <= minibatch_num_edges[1:])
+        is not shuffle
+    )
 
 
-def test_ItemSet_node_pairs():
+@pytest.mark.parametrize("batch_size", [1, 4])
+@pytest.mark.parametrize("shuffle", [True, False])
+@pytest.mark.parametrize("drop_last", [True, False])
+def test_ItemSet_node_pairs(batch_size, shuffle, drop_last):
     # Node pairs.
-    node_pairs = (torch.arange(0, 5), torch.arange(5, 10))
-    item_set = ItemSet(node_pairs)
-    for i, (src, dst) in enumerate(item_set):
-        assert node_pairs[0][i] == src
-        assert node_pairs[1][i] == dst
+    num_ids = 103
+    node_pairs = (torch.arange(0, num_ids), torch.arange(num_ids, num_ids * 2))
+    item_set = gb.ItemSet(node_pairs)
+    minibatch_sampler = gb.MinibatchSampler(
+        item_set, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last
+    )
+    src_ids = []
+    dst_ids = []
+    for i, (src, dst) in enumerate(minibatch_sampler):
+        is_last = (i + 1) * batch_size >= num_ids
+        if not is_last or num_ids % batch_size == 0:
+            assert len(src) == batch_size
+            assert len(dst) == batch_size
+        else:
+            if not drop_last:
+                assert len(src) == num_ids % batch_size
+                assert len(dst) == num_ids % batch_size
+            else:
+                assert False
+        src_ids.append(src)
+        dst_ids.append(dst)
+        assert torch.equal(src + num_ids, dst)
+    src_ids = torch.cat(src_ids)
+    dst_ids = torch.cat(dst_ids)
+    assert torch.all(src_ids[:-1] <= src_ids[1:]) is not shuffle
+    assert torch.all(dst_ids[:-1] <= dst_ids[1:]) is not shuffle
 
 
-def test_ItemSet_node_pairs_labels():
+@pytest.mark.parametrize("batch_size", [1, 4])
+@pytest.mark.parametrize("shuffle", [True, False])
+@pytest.mark.parametrize("drop_last", [True, False])
+def test_ItemSet_node_pairs_labels(batch_size, shuffle, drop_last):
     # Node pairs and labels
-    node_pairs = (torch.arange(0, 5), torch.arange(5, 10))
-    labels = torch.randint(0, 3, (5,))
-    item_set = ItemSet((node_pairs[0], node_pairs[1], labels))
-    for i, (src, dst, label) in enumerate(item_set):
-        assert node_pairs[0][i] == src
-        assert node_pairs[1][i] == dst
-        assert labels[i] == label
+    num_ids = 103
+    node_pairs = (torch.arange(0, num_ids), torch.arange(num_ids, num_ids * 2))
+    labels = torch.arange(0, num_ids)
+    item_set = gb.ItemSet((node_pairs[0], node_pairs[1], labels))
+    minibatch_sampler = gb.MinibatchSampler(
+        item_set, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last
+    )
+    src_ids = []
+    dst_ids = []
+    labels = []
+    for i, (src, dst, label) in enumerate(minibatch_sampler):
+        is_last = (i + 1) * batch_size >= num_ids
+        if not is_last or num_ids % batch_size == 0:
+            assert len(src) == batch_size
+            assert len(dst) == batch_size
+            assert len(label) == batch_size
+        else:
+            if not drop_last:
+                assert len(src) == num_ids % batch_size
+                assert len(dst) == num_ids % batch_size
+                assert len(label) == num_ids % batch_size
+            else:
+                assert False
+        src_ids.append(src)
+        dst_ids.append(dst)
+        labels.append(label)
+        assert torch.equal(src + num_ids, dst)
+        assert torch.equal(src, label)
+    src_ids = torch.cat(src_ids)
+    dst_ids = torch.cat(dst_ids)
+    labels = torch.cat(labels)
+    assert torch.all(src_ids[:-1] <= src_ids[1:]) is not shuffle
+    assert torch.all(dst_ids[:-1] <= dst_ids[1:]) is not shuffle
+    assert torch.all(labels[:-1] <= labels[1:]) is not shuffle
 
 
-def test_ItemSet_head_tail_neg_tails():
+@pytest.mark.parametrize("batch_size", [1, 4])
+@pytest.mark.parametrize("shuffle", [True, False])
+@pytest.mark.parametrize("drop_last", [True, False])
+def test_ItemSet_head_tail_neg_tails(batch_size, shuffle, drop_last):
     # Head, tail and negative tails.
-    heads = torch.arange(0, 5)
-    tails = torch.arange(5, 10)
-    neg_tails = torch.arange(10, 20).reshape(5, 2)
-    item_set = ItemSet((heads, tails, neg_tails))
+    num_ids = 103
+    num_negs = 2
+    heads = torch.arange(0, num_ids)
+    tails = torch.arange(num_ids, num_ids * 2)
+    neg_tails = torch.stack((heads + 1, heads + 2), dim=-1)
+    item_set = gb.ItemSet((heads, tails, neg_tails))
     for i, (head, tail, negs) in enumerate(item_set):
         assert heads[i] == head
         assert tails[i] == tail
-        assert_close(neg_tails[i], negs)
+        assert torch.equal(neg_tails[i], negs)
+    minibatch_sampler = gb.MinibatchSampler(
+        item_set, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last
+    )
+    head_ids = []
+    tail_ids = []
+    negs_ids = []
+    for i, (head, tail, negs) in enumerate(minibatch_sampler):
+        head_ids.append(head)
+        tail_ids.append(tail)
+        negs_ids.append(negs)
+        is_last = (i + 1) * batch_size >= num_ids
+        if not is_last or num_ids % batch_size == 0:
+            expected_batch_size = batch_size
+        else:
+            if not drop_last:
+                expected_batch_size = num_ids % batch_size
+            else:
+                assert False
+        assert len(head) == expected_batch_size
+        assert len(tail) == expected_batch_size
+        assert negs.dim() == 2
+        assert negs.shape[0] == expected_batch_size
+        assert negs.shape[1] == num_negs
+        assert torch.equal(head + num_ids, tail)
+        assert torch.equal(head + 1, negs[:, 0])
+        assert torch.equal(head + 2, negs[:, 1])
+    head_ids = torch.cat(head_ids)
+    tail_ids = torch.cat(tail_ids)
+    negs_ids = torch.cat(negs_ids)
+    assert torch.all(head_ids[:-1] <= head_ids[1:]) is not shuffle
+    assert torch.all(tail_ids[:-1] <= tail_ids[1:]) is not shuffle
+    assert torch.all(negs_ids[:-1, 0] <= negs_ids[1:, 0]) is not shuffle
+    assert torch.all(negs_ids[:-1, 1] <= negs_ids[1:, 1]) is not shuffle
 
 
-def test_DictItemSet_node_edge_ids():
+@pytest.mark.parametrize("batch_size", [1, 4])
+@pytest.mark.parametrize("shuffle", [True, False])
+@pytest.mark.parametrize("drop_last", [True, False])
+def test_DictItemSet_node_edge_ids(batch_size, shuffle, drop_last):
     # Node or edge IDs
+    num_ids = 205
     ids = {
-        ("user", "like", "item"): ItemSet(torch.arange(0, 5)),
-        ("user", "follow", "user"): ItemSet(torch.arange(0, 5)),
+        ("user", "like", "item"): gb.ItemSet(torch.arange(0, 99)),
+        ("user", "follow", "user"): gb.ItemSet(torch.arange(99, num_ids)),
     }
     chained_ids = []
     for key, value in ids.items():
         chained_ids += [(key, v) for v in value]
-    item_set = DictItemSet(ids)
-    for i, item in enumerate(item_set):
-        assert len(item) == 1
-        assert isinstance(item, dict)
-        assert chained_ids[i][0] in item
-        assert item[chained_ids[i][0]] == chained_ids[i][1]
+    item_set = gb.DictItemSet(ids)
+    minibatch_sampler = gb.MinibatchSampler(
+        item_set, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last
+    )
+    minibatch_ids = []
+    for i, batch in enumerate(minibatch_sampler):
+        is_last = (i + 1) * batch_size >= num_ids
+        if not is_last or num_ids % batch_size == 0:
+            expected_batch_size = batch_size
+        else:
+            if not drop_last:
+                expected_batch_size = num_ids % batch_size
+            else:
+                assert False
+        assert isinstance(batch, dict)
+        ids = []
+        for _, v in batch.items():
+            ids.append(v)
+        ids = torch.cat(ids)
+        assert len(ids) == expected_batch_size
+        minibatch_ids.append(ids)
+    minibatch_ids = torch.cat(minibatch_ids)
+    assert torch.all(minibatch_ids[:-1] <= minibatch_ids[1:]) is not shuffle
 
 
-def test_DictItemSet_node_pairs():
+@pytest.mark.parametrize("batch_size", [1, 4])
+@pytest.mark.parametrize("shuffle", [True, False])
+@pytest.mark.parametrize("drop_last", [True, False])
+def test_DictItemSet_node_pairs(batch_size, shuffle, drop_last):
     # Node pairs.
-    node_pairs = (torch.arange(0, 5), torch.arange(5, 10))
+    num_ids = 103
+    total_ids = 2 * num_ids
+    node_pairs_0 = (
+        torch.arange(0, num_ids),
+        torch.arange(num_ids, num_ids * 2),
+    )
+    node_pairs_1 = (
+        torch.arange(num_ids * 2, num_ids * 3),
+        torch.arange(num_ids * 3, num_ids * 4),
+    )
     node_pairs_dict = {
-        ("user", "like", "item"): ItemSet(node_pairs),
-        ("user", "follow", "user"): ItemSet(node_pairs),
+        ("user", "like", "item"): gb.ItemSet(node_pairs_0),
+        ("user", "follow", "user"): gb.ItemSet(node_pairs_1),
     }
-    expected_data = []
-    for key, value in node_pairs_dict.items():
-        expected_data += [(key, v) for v in value]
-    item_set = DictItemSet(node_pairs_dict)
-    for i, item in enumerate(item_set):
-        assert len(item) == 1
-        assert isinstance(item, dict)
-        assert expected_data[i][0] in item
-        assert item[expected_data[i][0]] == expected_data[i][1]
+    item_set = gb.DictItemSet(node_pairs_dict)
+    minibatch_sampler = gb.MinibatchSampler(
+        item_set, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last
+    )
+    src_ids = []
+    dst_ids = []
+    for i, batch in enumerate(minibatch_sampler):
+        is_last = (i + 1) * batch_size >= total_ids
+        if not is_last or total_ids % batch_size == 0:
+            expected_batch_size = batch_size
+        else:
+            if not drop_last:
+                expected_batch_size = total_ids % batch_size
+            else:
+                assert False
+        src = []
+        dst = []
+        for _, (v_src, v_dst) in batch.items():
+            src.append(v_src)
+            dst.append(v_dst)
+        src = torch.cat(src)
+        dst = torch.cat(dst)
+        assert len(src) == expected_batch_size
+        assert len(dst) == expected_batch_size
+        src_ids.append(src)
+        dst_ids.append(dst)
+        assert torch.equal(src + num_ids, dst)
+    src_ids = torch.cat(src_ids)
+    dst_ids = torch.cat(dst_ids)
+    assert torch.all(src_ids[:-1] <= src_ids[1:]) is not shuffle
+    assert torch.all(dst_ids[:-1] <= dst_ids[1:]) is not shuffle
 
 
-def test_DictItemSet_node_pairs_labels():
+@pytest.mark.parametrize("batch_size", [1, 4])
+@pytest.mark.parametrize("shuffle", [True, False])
+@pytest.mark.parametrize("drop_last", [True, False])
+def test_DictItemSet_node_pairs_labels(batch_size, shuffle, drop_last):
     # Node pairs and labels
-    node_pairs = (torch.arange(0, 5), torch.arange(5, 10))
-    labels = torch.randint(0, 3, (5,))
+    num_ids = 103
+    total_ids = 2 * num_ids
+    node_pairs_0 = (
+        torch.arange(0, num_ids),
+        torch.arange(num_ids, num_ids * 2),
+    )
+    node_pairs_1 = (
+        torch.arange(num_ids * 2, num_ids * 3),
+        torch.arange(num_ids * 3, num_ids * 4),
+    )
+    labels = torch.arange(0, num_ids)
     node_pairs_dict = {
-        ("user", "like", "item"): ItemSet(
-            (node_pairs[0], node_pairs[1], labels)
+        ("user", "like", "item"): gb.ItemSet(
+            (node_pairs_0[0], node_pairs_0[1], labels)
         ),
-        ("user", "follow", "user"): ItemSet(
-            (node_pairs[0], node_pairs[1], labels)
+        ("user", "follow", "user"): gb.ItemSet(
+            (node_pairs_1[0], node_pairs_1[1], labels + num_ids * 2)
         ),
     }
-    expected_data = []
-    for key, value in node_pairs_dict.items():
-        expected_data += [(key, v) for v in value]
-    item_set = DictItemSet(node_pairs_dict)
-    for i, item in enumerate(item_set):
-        assert len(item) == 1
-        assert isinstance(item, dict)
-        assert expected_data[i][0] in item
-        assert item[expected_data[i][0]] == expected_data[i][1]
+    item_set = gb.DictItemSet(node_pairs_dict)
+    minibatch_sampler = gb.MinibatchSampler(
+        item_set, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last
+    )
+    src_ids = []
+    dst_ids = []
+    labels = []
+    for i, batch in enumerate(minibatch_sampler):
+        is_last = (i + 1) * batch_size >= total_ids
+        if not is_last or total_ids % batch_size == 0:
+            expected_batch_size = batch_size
+        else:
+            if not drop_last:
+                expected_batch_size = total_ids % batch_size
+            else:
+                assert False
+        src = []
+        dst = []
+        label = []
+        for _, (v_src, v_dst, v_label) in batch.items():
+            src.append(v_src)
+            dst.append(v_dst)
+            label.append(v_label)
+        src = torch.cat(src)
+        dst = torch.cat(dst)
+        label = torch.cat(label)
+        assert len(src) == expected_batch_size
+        assert len(dst) == expected_batch_size
+        assert len(label) == expected_batch_size
+        src_ids.append(src)
+        dst_ids.append(dst)
+        labels.append(label)
+        assert torch.equal(src + num_ids, dst)
+        assert torch.equal(src, label)
+    src_ids = torch.cat(src_ids)
+    dst_ids = torch.cat(dst_ids)
+    labels = torch.cat(labels)
+    assert torch.all(src_ids[:-1] <= src_ids[1:]) is not shuffle
+    assert torch.all(dst_ids[:-1] <= dst_ids[1:]) is not shuffle
+    assert torch.all(labels[:-1] <= labels[1:]) is not shuffle
 
 
-def test_DictItemSet_head_tail_neg_tails():
+@pytest.mark.parametrize("batch_size", [1, 4])
+@pytest.mark.parametrize("shuffle", [True, False])
+@pytest.mark.parametrize("drop_last", [True, False])
+def test_DictItemSet_head_tail_neg_tails(batch_size, shuffle, drop_last):
     # Head, tail and negative tails.
-    heads = torch.arange(0, 5)
-    tails = torch.arange(5, 10)
-    neg_tails = torch.arange(10, 20).reshape(5, 2)
-    item_set = ItemSet((heads, tails, neg_tails))
+    num_ids = 103
+    total_ids = 2 * num_ids
+    num_negs = 2
+    heads = torch.arange(0, num_ids)
+    tails = torch.arange(num_ids, num_ids * 2)
+    neg_tails = torch.stack((heads + 1, heads + 2), dim=-1)
     data_dict = {
-        ("user", "like", "item"): ItemSet((heads, tails, neg_tails)),
-        ("user", "follow", "user"): ItemSet((heads, tails, neg_tails)),
+        ("user", "like", "item"): gb.ItemSet((heads, tails, neg_tails)),
+        ("user", "follow", "user"): gb.ItemSet((heads, tails, neg_tails)),
     }
-    expected_data = []
-    for key, value in data_dict.items():
-        expected_data += [(key, v) for v in value]
-    item_set = DictItemSet(data_dict)
-    for i, item in enumerate(item_set):
-        assert len(item) == 1
-        assert isinstance(item, dict)
-        assert expected_data[i][0] in item
-        assert_close(item[expected_data[i][0]], expected_data[i][1])
-"""
+    item_set = gb.DictItemSet(data_dict)
+    minibatch_sampler = gb.MinibatchSampler(
+        item_set, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last
+    )
+    head_ids = []
+    tail_ids = []
+    negs_ids = []
+    for i, batch in enumerate(minibatch_sampler):
+        is_last = (i + 1) * batch_size >= total_ids
+        if not is_last or total_ids % batch_size == 0:
+            expected_batch_size = batch_size
+        else:
+            if not drop_last:
+                expected_batch_size = total_ids % batch_size
+            else:
+                assert False
+        head = []
+        tail = []
+        negs = []
+        for _, (v_head, v_tail, v_negs) in batch.items():
+            head.append(v_head)
+            tail.append(v_tail)
+            negs.append(v_negs)
+        head = torch.cat(head)
+        tail = torch.cat(tail)
+        negs = torch.cat(negs)
+        assert len(head) == expected_batch_size
+        assert len(tail) == expected_batch_size
+        assert len(negs) == expected_batch_size
+        head_ids.append(head)
+        tail_ids.append(tail)
+        negs_ids.append(negs)
+        assert negs.dim() == 2
+        assert negs.shape[0] == expected_batch_size
+        assert negs.shape[1] == num_negs
+        assert torch.equal(head + num_ids, tail)
+        assert torch.equal(head + 1, negs[:, 0])
+        assert torch.equal(head + 2, negs[:, 1])
+    head_ids = torch.cat(head_ids)
+    tail_ids = torch.cat(tail_ids)
+    negs_ids = torch.cat(negs_ids)
+    assert torch.all(head_ids[:-1] <= head_ids[1:]) is not shuffle
+    assert torch.all(tail_ids[:-1] <= tail_ids[1:]) is not shuffle
+    assert torch.all(negs_ids[:-1] <= negs_ids[1:]) is not shuffle
